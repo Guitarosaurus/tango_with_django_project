5a9b6d533f79aec71c8cdae9179e2138ec93b0ba
Hannah Weir <2881720w@student.gla.ac.uk>
2025-01-31 11:38:41 +0000

    Passed chapter 3 tests and static files added

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!\nAbout page: <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page\n" + "Home page: <a href='/rango/'>About</a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
